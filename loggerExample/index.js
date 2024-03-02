2bf9c03f1f66f1a07f1e68c5b1d8f9ac9c529702
smerch88 <maxarmyk@gmail.com>
2024-03-02 12:16:53 +0200

    Revert "Revert "Merge pull request #2 from smerch88/t-2""
    This reverts commit 2881db8226771b58b40cc80dcd2a4c1eb23f8c96.

---
+++ loggerExample/index.js
@@ -0,0 +1,12 @@
+import color from "./color.js"
+import logger from "../lib/logger/logger.js";
+import dotenv from "dotenv";
+dotenv.config()
+
+const log = logger.getLogger("app.js");
+
+log.info(color)
+log.error(color)
+log.warn(color)
+log.trace(color)
+log.trace(color)
